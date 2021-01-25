70f3916f7ebb1ac9f7e0e9c5d067136aa28e5dce
Evan Wallace <evan.exe@gmail.com>
2021-01-24 23:11:08 -0800

    ci: attempt to fix windows-only wasm issues

--- internal/fs/iswin_wasm.go
+++ internal/fs/iswin_wasm.go
@@ -18,7 +18,7 @@ func checkIfWindows() bool {
 		// "windir" environment variable is set. This is a workaround for a bug in
 		// Go's WebAssembly support: https://github.com/golang/go/issues/43768.
 		for _, env := range os.Environ() {
-			if strings.EqualFold(env, "windir") {
+			if equals := strings.IndexByte(env, '='); equals != -1 && strings.EqualFold(env[:equals], "windir") {
 				cachedIfWindows = true
 				break
 			}
